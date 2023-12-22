7df12e40e9080d0e2742ea05e17d394d1c4742ca
David Hotham <david.hotham@blueyonder.co.uk>
2023-12-22 19:29:45 +0000

    micro-optimizing day 16

--- advent/day16.py
+++ advent/day16.py
@@ -1,28 +1,26 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeAlias
+from typing import TypeAlias
 
 from attrs import field, frozen
 
 from advent.utils import Coord2, data_dir
 
-if TYPE_CHECKING:
-    from collections.abc import Iterator
-
-Grid: TypeAlias = dict[Coord2, str]
+Grid: TypeAlias = list[str]
 State: TypeAlias = tuple[Coord2, Coord2]
 
 NORTH = (-1, 0)
 SOUTH = (1, 0)
 EAST = (0, 1)
 WEST = (0, -1)
+DIRECTION_INDEX = {NORTH: 0, SOUTH: 1, EAST: 2, WEST: 3}
 
 TURNS = {
-    "-": {NORTH: [EAST, WEST], SOUTH: [EAST, WEST], EAST: [EAST], WEST: [WEST]},
-    "|": {NORTH: [NORTH], SOUTH: [SOUTH], EAST: [NORTH, SOUTH], WEST: [NORTH, SOUTH]},
-    "/": {NORTH: [EAST], SOUTH: [WEST], EAST: [NORTH], WEST: [SOUTH]},
-    "\\": {NORTH: [WEST], SOUTH: [EAST], EAST: [SOUTH], WEST: [NORTH]},
-    ".": {NORTH: [NORTH], SOUTH: [SOUTH], EAST: [EAST], WEST: [WEST]},
+    "-": [[EAST, WEST], [EAST, WEST], [EAST], [WEST]],
+    "|": [[NORTH], [SOUTH], [NORTH, SOUTH], [NORTH, SOUTH]],
+    "/": [[EAST], [WEST], [NORTH], [SOUTH]],
+    "\\": [[WEST], [EAST], [SOUTH], [NORTH]],
+    ".": [[NORTH], [SOUTH], [EAST], [WEST]],
 }
 
 
@@ -33,49 +31,48 @@ class Layout:
     cols: int = field(init=False)
 
     def __attrs_post_init__(self) -> None:
-        object.__setattr__(self, "rows", 1 + max(r for r, _ in self.grid))
-        object.__setattr__(self, "cols", 1 + max(c for _, c in self.grid))
+        rows = len(self.grid)
+        cols = len(self.grid[0])
+        object.__setattr__(self, "rows", rows)
+        object.__setattr__(self, "cols", cols)
 
     @staticmethod
     def from_string(text: str) -> Layout:
-        grid: Grid = {}
-        for row, line in enumerate(text.splitlines()):
-            for col, char in enumerate(line):
-                grid[(row, col)] = char
-
+        grid = text.splitlines()
         return Layout(grid)
 
-    def neighbours(self, state: State) -> Iterator[State]:
-        place, direction = state
-        content = self.grid[place]
-        (row, col) = place
-        for new_direction in TURNS[content][direction]:
-            drow, dcol = new_direction
-            new_row, new_col = row + drow, col + dcol
-
-            if not 0 <= new_row < self.rows:
-                continue
-
-            if not 0 <= new_col < self.cols:
-                continue
-
-            yield ((new_row, new_col), new_direction)
-
     def energize(self, start: State = ((0, 0), EAST)) -> int:
-        visited: set[State] = set()
+        count = 0
+        visited = [0] * self.rows * self.cols
         stack = [start]
         while stack:
             state = stack.pop()
-            if state in visited:
+            (row, col), direction = state
+
+            pos_index = row * self.cols + col
+            dir_index = DIRECTION_INDEX[direction]
+            dir_bit = 1 << dir_index
+            if visited[pos_index] & dir_bit:
                 continue
 
-            visited.add(state)
+            if visited[pos_index] == 0:
+                count += 1
 
-            neighbours = self.neighbours(state)
-            stack.extend(neighbours)
+            visited[pos_index] |= dir_bit
 
-        places = {place for place, _ in visited}
-        return len(places)
+            content = self.grid[row][col]
+            for drow, dcol in TURNS[content][dir_index]:
+                new_row, new_col = row + drow, col + dcol
+
+                if not 0 <= new_row < self.rows:
+                    continue
+
+                if not 0 <= new_col < self.cols:
+                    continue
+
+                stack.append(((new_row, new_col), (drow, dcol)))
+
+        return count
 
     def most_energized(self) -> int:
         lefts = (((row, 0), EAST) for row in range(self.rows))
